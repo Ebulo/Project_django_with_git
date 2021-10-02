106c1b35c19aad940f8bceea7da0b9cfc671d782
Bishant Nayak <48652521+Ebulo@users.noreply.github.com>
2021-10-03 01:16:27 +0530

    created a strarter file

---
+++ starter.py
@@ -0,0 +1,9 @@
+def greet(greet):
+  if greet == "morning":
+    print("Good morning sir! welcome to the club")
+  elif greet == "afternoon":
+    print("A very good afternoon sir!")
+  elif greet == "goodnight":
+    print("Good night sir! Sweet dreams")
+  else:
+    print("Hello! how are you")
